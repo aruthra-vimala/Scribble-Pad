51cb3ce500b847d6956ac0db30137d7f78cbf7a9
Aruthra Vimala K L <aruthravimala.kl2023@vitstudent.ac.in>
2024-11-09 20:00:17 +0530

    Update file.js

--- file.js
+++ file.js
@@ -1,4 +1,6 @@
 const video = document.getElementById('video');
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
 const output = document.getElementById('output');
 
 // Access the user's webcam
@@ -15,7 +17,6 @@ const loadHandposeModel = async () => {
     const model = await handpose.load();
     console.log('Handpose model loaded.');
 
-    // Start detecting gestures
     video.onloadeddata = () => {
         detectHandGestures(model);
     };
@@ -25,8 +26,12 @@ const loadHandposeModel = async () => {
 const detectHandGestures = async (model) => {
     while (true) {
         const predictions = await model.estimateHands(video);
+        ctx.clearRect(0, 0, canvas.width, canvas.height); // Clear the canvas
+
         if (predictions.length > 0) {
             const hand = predictions[0];
+            console.log('Hand landmarks:', hand.landmarks);
+            drawHand(hand.landmarks);
             output.innerText = recognizeGesture(hand.landmarks);
         } else {
             output.innerText = "No hand detected";
@@ -35,34 +40,47 @@ const detectHandGestures = async (model) => {
     }
 };
 
+// Function to draw hand landmarks
+const drawHand = (landmarks) => {
+    ctx.fillStyle = 'red';
+    ctx.strokeStyle = 'red';
+    ctx.lineWidth = 2;
+
+    // Draw points
+    landmarks.forEach(point => {
+        ctx.beginPath();
+        ctx.arc(point[0], point[1], 5, 0, 2 * Math.PI);
+        ctx.fill();
+    });
+
+    // Draw lines between fingertips
+    const fingers = [4, 8, 12, 16, 20];
+    for (let i = 0; i < fingers.length - 1; i++) {
+        const start = landmarks[fingers[i]];
+        const end = landmarks[fingers[i + 1]];
+        ctx.beginPath();
+        ctx.moveTo(start[0], start[1]);
+        ctx.lineTo(end[0], end[1]);
+        ctx.stroke();
+    }
+};
+
 // Function to recognize gestures
 const recognizeGesture = (landmarks) => {
-    const gestures = {
-        "Thumbs Up": isThumbsUp(landmarks),
-        "Peace": isPeace(landmarks),
-        "Fist": isFist(landmarks)
-    };
+    const thumbTip = landmarks[4];
+    const indexTip = landmarks[8];
+    const middleTip = landmarks[12];
+    const ringTip = landmarks[16];
+    const pinkyTip = landmarks[20];
 
-    for (let [gesture, detected] of Object.entries(gestures)) {
-        if (detected) return gesture;
+    if (thumbTip[1] < indexTip[1] && 
+        thumbTip[1] < middleTip[1] && 
+        thumbTip[1] < ringTip[1] && 
+        thumbTip[1] < pinkyTip[1]) {
+        return "Thumbs Up!";
+    } else {
+        return "Gesture not recognized";
     }
-    return "Gesture not recognized";
-};
-
-// Example gesture recognition functions
-const isThumbsUp = (landmarks) => {
-    const [thumbTip, indexTip, middleTip, ringTip, pinkyTip] = [landmarks[4], landmarks[8], landmarks[12], landmarks[16], landmarks[20]];
-    return (thumbTip[1] < indexTip[1] && thumbTip[1] < middleTip[1] && thumbTip[1] < ringTip[1] && thumbTip[1] < pinkyTip[1]);
-};
-
-const isPeace = (landmarks) => {
-    const [indexTip, middleTip, ringTip, pinkyTip] = [landmarks[8], landmarks[12], landmarks[16], landmarks[20]];
-    return (indexTip[1] < ringTip[1] && middleTip[1] < ringTip[1] && ringTip[1] > pinkyTip[1]);
-};
-
-const isFist = (landmarks) => {
-    const [thumbTip, indexTip, middleTip, ringTip, pinkyTip] = [landmarks[4], landmarks[8], landmarks[12], landmarks[16], landmarks[20]];
-    return (thumbTip[1] > indexTip[1] && thumbTip[1] > middleTip[1] && thumbTip[1] > ringTip[1] && thumbTip[1] > pinkyTip[1]);
 };
 
 // Initialize the model
